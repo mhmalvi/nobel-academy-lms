efc307c9f3dfc576dfcf2a05acecc58921b83c83
ANrajin <dev.quadque@gmail.com>
2021-07-17 18:34:31 +0600

    user code refactored

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -29,54 +29,22 @@ class UsersController extends Controller
     public function store(CreateUserRequest $request)
     {
         try {
-            $file = null;
+            $user = $request->store();
 
-            if ($request->hasFile('avatar')) {
-                /**
-                 * Check if derectory exist or not
-                 * Create a new directory if not exist
-                 */
-                if (!Storage::exists("public/users")) {
-                    Storage::makeDirectory("public/users");
-                }
-
-                $image = $request->file('avatar');
-                $imgExtension = $image->getClientOriginalExtension();
-
-                $file = date('dmy-hms') . '.' . $imgExtension;
-
-                //Store the file after saving it to the databse
-                Storage::putFileAs('public/users', $image, $file);
+            if ($user) {
+                $request->saveInfo($user);
             }
 
-            $user = User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => $request->password,
-                'photo' => $file,
-                'user_type' => $request->user_type,
-            ]);
-
-            if ($user->id) {
-                UserInfo::create([
-                    'user_id' => $user->id,
-                    'firstname' => $request->fName,
-                    'lastname' => $request->lName,
-                    'phone' => $request->phone,
-                    'address' => $request->address,
+            if ($user->user_type == 'student' && $request->has('course_id')) {
+                Enrollment::create([
+                    'student_id' => $user->id,
+                    'course_id' => $this->course_id,
                 ]);
 
-                if ($user->user_type == 'student') {
-                    Enrollment::create([
-                        'student_id' => $user->id,
-                        'course_id' => $request->course_id,
-                    ]);
-
-                    return redirect()->route('admin.assign', $user->id);
-                }
+                return redirect()->route('admin.assign', $user->id);
+            } else {
+                return back();
             }
-
-            return back();
         } catch (\Throwable $th) {
             /**
              * Return the exceptions
--- app/Http/Requests/CreateUserRequest.php
+++ app/Http/Requests/CreateUserRequest.php
@@ -4,6 +4,9 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use App\Models\User;
+use Illuminate\Support\Facades\Storage;
+use App\Models\UserInfo;
 
 class CreateUserRequest extends FormRequest
 {
@@ -39,5 +42,58 @@ class CreateUserRequest extends FormRequest
                 'max:1048'
             ],
         ];
+    }
+
+
+    /**
+     * Store User
+     */
+    public function store()
+    {
+        return
+            User::create([
+                'name' => $this->name,
+                'email' => $this->email,
+                'password' => $this->password,
+                'photo' => ($this->hasFile('avatar')) ? $this->avater($this->file('avatar')) : null,
+                'user_type' => $this->user_type,
+            ]);
+    }
+
+
+    /**
+     * Store Avater
+     */
+    public function avater($avater)
+    {
+        /**
+         * Check if derectory exist or not
+         * Create a new directory if not exist
+         */
+        if (!Storage::exists("public/users")) {
+            Storage::makeDirectory("public/users");
+        }
+
+        $image = $avater;
+        $imgExtension = $image->getClientOriginalExtension();
+
+        $file = date('dmy-hms') . '.' . $imgExtension;
+
+        //Store the file after saving it to the databse
+        Storage::putFileAs('public/users', $image, $file);
+
+        return $file;
+    }
+
+
+    public function saveInfo($user)
+    {
+        UserInfo::create([
+            'user_id' => $user->id,
+            'firstname' => $this->fName,
+            'lastname' => $this->lName,
+            'phone' => $this->phone,
+            'address' => $this->address,
+        ]);
     }
 }
